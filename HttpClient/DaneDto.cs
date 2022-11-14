9bc8b188e2efebcbf0d5392def072e9251be5896
MstrJ <jakubkruliczak@gmail.com>
2022-11-14 15:03:39 +0100

    poprawiona logika kodu

--- HttpClient/DaneDto.cs
+++ HttpClient/DaneDto.cs
@@ -12,49 +12,6 @@ class DaneDto : IMethods
 
     private static List<Post> _posts = new List<Post>();
     public static string uri = "https://localhost:7294/api/Post";
-    public static string GetAll(out int[] ids, out string[] titles, out string[] contents)
-    {
-        var task = client.GetAsync(uri);
-        task.Wait();
-
-        if (task.IsCompleted)
-        {
-            var result = task.Result;
-            if (result.IsSuccessStatusCode)
-            {
-                var message = result.Content.ReadAsStringAsync().Result;
-                List<Post> info = JsonConvert.DeserializeObject<List<Post>>(message);
-
-                string[] listaInfo = new string[info.Count()];
-                string wszystko = "";
-                ids = new int[info.Count()];
-                titles = new string[info.Count()];
-                contents = new string[info.Count()];
-                //for (int i = 0; i < listaInfo.Length; i++)
-                //{
-                //    wszystko = string.Join("\n", info);
-                //}
-                ///raw^
-
-                for (int i = 0; i < info.Count(); i++)
-                {
-                    ids[i] = info[i].Id;
-                    titles[i] = info[i].Title;
-                    contents[i] = info[i].Content;
-                    var items = $"Id: {ids[i]}\tTitle: {titles[i]}\tContent: {contents[i]}";
-                    listaInfo[i] = items;
-                    wszystko += $"{listaInfo[i]}\n";
-
-                    Post post = new(info[i].Id, info[i].Title, info[i].Content);
-                    _posts.Add(post);
-                }
-
-                return wszystko;
-            }
-        }
-
-        throw new Exception($"{task}!=IsCompleted");
-    }
 
     public string GetAll()
     {
@@ -91,26 +48,31 @@ class DaneDto : IMethods
 
     public string GetById(int id)
     {
-        var task = client.GetAsync($"{uri}/{id}");
-        task.Wait();
-
-        if (task.IsCompleted)
+        bool logic = _posts.FirstOrDefault(x => x.Id == id) == null ? false : true;
+        if (logic)
         {
-            var result = task.Result;
-            if (result.IsSuccessStatusCode)
+            var task = client.GetAsync($"{uri}/{id}");
+            task.Wait();
+
+            if (task.IsCompleted)
             {
-                var message = result.Content.ReadAsStringAsync().Result;
-                Post info = JsonConvert.DeserializeObject<Post>(message);
-               
-                var Title = info.Title;
-                var Content = info.Content;
+                var result = task.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var message = result.Content.ReadAsStringAsync().Result;
+                    Post info = JsonConvert.DeserializeObject<Post>(message);
 
-                string wszystko = $"Id: {id}\tTitle: {Title}\tContent: {Content}";
-                return wszystko;
+                    var Title = info.Title;
+                    var Content = info.Content;
+
+                    string wszystko = $"Id: {id}\tTitle: {Title}\tContent: {Content}";
+                    return wszystko;
+                }
             }
-        }
 
-        throw new Exception($"{task}!=IsCompleted");
+            throw new Exception($"{task}!=IsCompleted");
+        }
+        return "Nie ma takie Id!";
     }
 
     public string Post(NewPost newPost)
