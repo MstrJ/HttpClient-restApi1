ebf0164c04fe037f7ff583f7f04030b28e2c959a
MstrJ <jakubkruliczak@gmail.com>
2022-11-15 13:52:12 +0100

    aktualizacja pliku gitignore

---
+++ HttpClient/IMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DaneDto;
+
+interface IMethods
+{
+    public string GetAll(Direction? direction,DirectionBy? directionBy);
+    public string GetById(int id);
+    public string Post(NewPost newPost);
+    public string Put(UpdatePost updatePost);
+    public string Delete(int id);
+}
